97e9abf8cc58dc7fc14f9304ce11fd96f5b35410
josevictor48 <143837861+josevictor48@users.noreply.github.com>
2024-09-30 08:05:31 -0300

    Add files via upload

---
+++ flashcard-aula_10 (2)/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Fisica',
+    'Determine a formula do MUV',
+    'S-S0= V0.T a.t²/2'
+)
+
+criaCartao(
+    'Matematica',
+    'Oque é forma canonica da função quadratica?',
+    'é a formula que determina o xV e o yV'
+)
+
+criaCartao(
+    'português',
+    'oque é diacritica',
+    'é quando a letra serve somente para torna a junção das letras em uma'
+)
+
+criaCartao(
+    'quimica',
+    'oque é substancia pura?',
+    'a substancia pura se trata de somente uma, como pegamos um copo de água se tiver somente a água, será uma pura '
+)
